8a04aea2a8c3cbbfaecbe69f4cd1f41fe4d2eeb6
ignaziopedone <i.pedone@live.it>
2018-10-05 14:59:07 +0200

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,2 +1,9 @@
+if [ $1 == "user" ]
+then
+sudo sh -c 'cat append_user.sh >> /etc/network/interfaces.d/50-cloud-init.cfg'
+sudo sh -c '/etc/init.d/networking restart'
+exit 1
+fi
+fi
 sudo sh -c 'cat append.sh >> /etc/network/interfaces.d/50-cloud-init.cfg'
 sudo sh -c '/etc/init.d/networking restart'
