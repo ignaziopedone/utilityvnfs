c7f3421e6a0672b067fb4ca90164efbb2bbc5a4a
ignaziopedone <i.pedone@live.it>
2018-10-08 14:30:08 +0200

    Add script ini_networking
    This commit have the aim to introduce a new script for internal network configuration of the vnfs.

---
+++ init_networking.sh
@@ -0,0 +1,11 @@
+sudo chmod +x network.sh
+./network.sh
+echo "Ifaces configured."
+
+sudo brctl addbr br0
+sudo brctl addif ens4 ens5
+sudo ifconfig br0 up
+echo "Bridge between in-out interfaces configured and up."
+
+sudo modprobe br_netfilter
+echo "Loading kernel module for iptables setting on a bridge."
