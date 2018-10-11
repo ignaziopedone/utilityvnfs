d1988f0716c6cfa4625724d3203721439ca4e70c
ignaziopedone <i.pedone@live.it>
2018-10-11 16:51:45 +0200

    fix an error in bridge creation

--- init_networking.sh
+++ init_networking.sh
@@ -3,7 +3,7 @@ sudo chmod +x network.sh
 echo "Ifaces configured."
 
 sudo brctl addbr br0
-sudo brctl addif ens4 ens5
+sudo brctl addif br0 ens4 ens5
 sudo ifconfig br0 up
 echo "Bridge between in-out interfaces configured and up."
 
